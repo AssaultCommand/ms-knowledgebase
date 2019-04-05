3c6a5acd2e41b5a2c14e71e29eccc4a49e5959e9
Z El-Mariami <highaeonkratex@gmail.com>
2019-04-05 15:10:35 +0200

    Categoryindex dynamic changes

--- Website/categoryindex.php
+++ Website/categoryindex.php
@@ -8,7 +8,7 @@
 	ob_start();
 ?>
 
-<!-- <section class="page-title">
+<section class="page-title">
 	<section class="title-area">
 		<div class="container">
 			<div class="row">
@@ -32,137 +32,64 @@
 <section class="page-content">
 	<section class="page-main">
 		<div class="container">
-			<div class="row">
-				<div class="col-sm-12 categoryItem">
-					<h3 class="indexResultName">
-						Google Plus Button
-					</h3>
+			<div id="category-items" class="row">
 
-					<div class="languageDescriptor">HTML-JS</div>
-					
-					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-				</div>
-				<div class="col-sm-12 categoryItem">
-					<h3 class="indexResultName">
-						Facebook Sharing Button
-					</h3>
-
-					<div class="languageDescriptor">HTML-JS</div>
-					
-					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-				</div>
-				<div class="col-sm-12 categoryItem">
-					<h3 class="indexResultName">
-						Twitter Sharing Button
-					</h3>
-
-					<div class="languageDescriptor">HTML-JS</div>
-
-					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-				</div>
-				<div class="col-sm-12 categoryItem">
-					<h3 class="indexResultName">
-						Snapchat Share Post BUtton
-					</h3>
-
-					<div class="languageDescriptor">HTML-JS</div>
-
-					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-				</div>
-				<div class="col-sm-12 categoryItem">
-					<h3 class="indexResultName">
-						Share To Whatsapp / Skype / Discord / Etc.
-					</h3>
-
-					<div class="languageDescriptor">HTML-JS</div>
-
-					'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-				</div>
 			</div>
 		</div>
 	</section>
 
 		<section class="page-sidebar">
 			<aside class="metaInfo">
-				<h4 class="sidePanelTitle">Languages</h4>
-				<div class="sideBarLanguages">
-					<div>
-					<input type="checkbox" id="phpcheckbox">
-						<label for="phpcheckbox">
-							<div class="checkbox"></div>
-							<span>PHP</span>
-						</label>
-					<input type="checkbox" id="htmlcheckbox">
-						<label for="htmlcheckbox">
-							<div class="checkbox"></div>
-							<span>HTML</span>
-						</label>
-					<input type="checkbox" id="javascriptcheckbox">
-						<label for="javascriptcheckbox">
-							<div class="checkbox"></div>
-							<span>Javascript</span>
-						</label>
-					<input type="checkbox" id="csscheckbox">
-						<label for="csscheckbox">
-							<div class="checkbox"></div>
-							<span>CSS</span>
-						</label>
-					</div>		
+				<h4 class="sidePanelTitle">Authors</h4>
+				<div class="sideBarAuthors">
+
+					<form autocomplete="off" action="">
+						<div class="autocomplete">
+							<input id="searchAuthor" type="text" name="myCountry" placeholder="Zoek op auteur">
+						</div>
+					</form>
+
 				</div>
+			</aside>
+			<script>
+				autocomplete(document.getElementById("searchAuthor"), authors);
+			</script>
+		<section class="page-sidebar">
+			<aside class="metaInfo">
+				<h4 class="sidePanelTitle">Languages</h4>
+				<div id="sideBarLanguages"></div>
 			</aside>
 			<aside class="metaInfo">
 				<h4 class="sidePanelTitle">Framework</h4>
-				<div class="sideBarLanguages">
-					<div>
-					<input type="checkbox" id="laravelcheckbox">
-						<label for="laravelcheckbox">
-							<div class="checkbox"></div>
-							<span>Laravel</span>
-						</label>
-					<input type="checkbox" id="composercheckbox">
-						<label for="composercheckbox">
-							<div class="checkbox"></div>
-							<span>Composer</span>
-						</label>
-					<input type="checkbox" id="jquerycheckbox">
-						<label for="jquerycheckbox">
-							<div class="checkbox"></div>
-							<span>jQuery</span>
-						</label>
-					<input type="checkbox" id="angularcheckbox">
-						<label for="angularcheckbox">
-							<div class="checkbox"></div>
-							<span>Angular</span>
-						</label>
-					</div>		
-				</div>
+				<div id="sideBarFrameworks"></div>
 			</aside>
 		</section>
-</section> -->
+</section>
 
 
-<div id="wrapper">	
-</div>
+
 
 
 <script>
-	var authors = []; 
+
+	var authors = [];
 
 	$.ajax({
-	  url: window.options.website.url + 'assets/php/data.php?data=users',
-	  type: 'get',
-	  dataType: 'json',
-	  async: false,
-	  success: function(data) {
-	  }
+		url: window.options.website.url + 'assets/php/data.php?data=users',
+		type: 'get',
+		dataType: 'json',
+		async: false
 	})
 	.done(function(data) {
-	  authors = data.data;
+		authors = data.data;
 	});
 
-    load_data_template('category-index', '#wrapper', options.website.url + 'assets/php/data.php', {'data': 'page', 'slug':'facebook-like-button'}, 'append');
 
+    load_data_template('category-item', '#category-items', options.website.url + 'assets/php/data.php', {'data': 'category_items'}, 'append');
 
+    load_data_template('filter-language', '#sideBarLanguages', options.website.url + 'assets/php/data.php', {'data': 'languages'}, 'append');
+
+    load_data_template('filter-framework', '#sideBarFrameworks', options.website.url + 'assets/php/data.php', {'data': 'frameworks'}, 'append');
 </script>
 
 
